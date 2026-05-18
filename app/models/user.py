d6c2efb0321516142686a323a6cd8c07e2293217
diogo thomaz <105013915+DiogoThomaz@users.noreply.github.com>
2026-05-18 19:40:57 -0300

    feat: add user model

---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+from app.db.session import engine
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, default=datetime.utcnow)
